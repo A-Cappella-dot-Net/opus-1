50e20c8e504c6db8005dc242c8758de7ca370042
Vladimir Ivanov <puiuvlad@users.noreply.github.com>
2025-08-07 06:59:43 -0400

    rename top level project from a-cappella to opus.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
-rootProject.name = "a-cappella"
+rootProject.name = "opus.1"
 include("continuo", "cembalo", "exchange")
