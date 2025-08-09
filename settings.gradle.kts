c9b286a6c7ba66e65d590960ae0e7be331bb63fc
Vladimir Ivanov <puiuvlad@users.noreply.github.com>
2025-08-09 09:24:16 -0400

    rename project name from opus.1 to opus-1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
-rootProject.name = "opus.1"
+rootProject.name = "opus-1"
 include("continuo", "cembalo", "exchange")
