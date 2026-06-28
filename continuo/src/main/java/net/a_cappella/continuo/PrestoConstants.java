bbd6359bd9b81451806176d6f2654665e2250020
Vladimir Ivanov <puiuvlad@users.noreply.github.com>
2026-06-28 12:45:00 -0400

    misc changes

--- continuo/src/main/java/net/a_cappella/continuo/PrestoConstants.java
+++ continuo/src/main/java/net/a_cappella/continuo/PrestoConstants.java
@@ -17,18 +17,18 @@
 package net.a_cappella.continuo;
 
 public interface PrestoConstants {
-    int FORCE_DISCONNECT      = -2;
-    int SERIAL                = -1;
-    int BYTES                 = 0;
-    int VERSIONED_STRING_MSG  = 1;
-    int REGISTRATION_REQUEST  = 2;
-    int REGISTRATION_RESPONSE = 3;
-    int VOTE_MSG              = 4;
-    int FT_MEMBER             = 5;
-    int FT_MONITOR            = 6;
-    int SOURCES_PORTS         = 7;
-    int PATHS_SUBJECTS        = 8;
-    int TEST_MSG              = 9;
+    int FORCE_DISCONNECT      = 1;
+    int SERIAL                = 2;
+    int BYTES                 = 3;
+    int VERSIONED_STRING_MSG  = 4;
+    int REGISTRATION_REQUEST  = 5;
+    int REGISTRATION_RESPONSE = 6;
+    int VOTE_MSG              = 7;
+    int FT_MEMBER             = 8;
+    int FT_MONITOR            = 9;
+    int SOURCES_PORTS         = 10;
+    int PATHS_SUBJECTS        = 11;
+    int TEST_MSG              = 12;
 
     // the below types must match the ones in presto-aeron/src/main/resources/schema.xml
     int TYPE_FT_MEMBER   = 101;
@@ -36,11 +36,10 @@ public interface PrestoConstants {
     int TYPE_SNP         = 103;
     int TYPE_SNP_TIMEOUT = 104;
     int TYPE_MAP         = 105;
-    int TYPE_MONITOR     = 106;
-    int TYPE_PING        = 107;
-    int TYPE_TEST        = 108;
-    int TYPE_CACHE_CMD   = 109;
-    int TYPE_SEQ_NO      = 110;
+    int TYPE_PING        = 106;
+    int TYPE_TEST        = 107;
+    int TYPE_CACHE_CMD   = 108;
+    int TYPE_SEQ_NO      = 109;
 
     String SUBJ_FT_MEMBER   = "ft.member";
     String SUBJ_FT_MONITOR  = "ft.monitor";
@@ -54,6 +53,4 @@ public interface PrestoConstants {
 
     char YES = 'Y';
     char NO = 'N';
-
-    String NULL_SUBJECT = "null";
 }
